37e4bd7179049ddcc57d63b50b2fb9c31333f48b
aldrich <nocheblanca92@gmail.com>
2025-05-14 16:59:04 -0600

    quitar session de signin

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,13 +1,12 @@
-import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
 import { LoginForm } from "../../components/login/login-form";
 
 export default async function LoginPage() {
-  const session = await auth();
+  // const session = await auth();
+  //
+  // if (session?.user) {
+  //   redirect("/admin");
+  // }
 
-  if (session?.user) {
-    redirect("/admin");
-  }
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-sm">
