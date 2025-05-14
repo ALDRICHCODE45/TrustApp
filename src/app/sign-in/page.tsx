47b144bffd769f7cfbe43d41f1b21baa3cebd0e9
aldrich <nocheblanca92@gmail.com>
2025-05-14 17:33:23 -0600

    noStore login

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,11 +1,15 @@
+import { auth } from "@/lib/auth";
 import { LoginForm } from "../../components/login/login-form";
+import { redirect } from "next/navigation";
+import { unstable_noStore } from "next/cache";
 
 export default async function LoginPage() {
-  // const session = await auth();
-  //
-  // if (session?.user) {
-  //   redirect("/admin");
-  // }
+  unstable_noStore();
+  const session = await auth();
+
+  if (session?.user) {
+    redirect("/admin");
+  }
 
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
