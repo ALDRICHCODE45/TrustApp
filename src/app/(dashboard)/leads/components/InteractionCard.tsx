51739a1403e41bbafed83dfc3513838f2963d822
Aldrich <nocheblanca92@gmail.com>
2025-05-14 15:47:24 -0600

    revision final

--- src/app/(dashboard)/leads/components/InteractionCard.tsx
+++ src/app/(dashboard)/leads/components/InteractionCard.tsx
@@ -33,13 +33,17 @@ import { es } from "date-fns/locale";
 import {
   Download,
   Edit,
+  FileText,
   Loader2,
   MoreVertical,
   PaperclipIcon,
   Trash2,
+  X,
 } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
+import { Attachment } from "./ContactCard";
+import { replaceFile, uploadFile } from "@/actions/files/actions";
 
 interface Props {
   interaction: ContactInteractionWithRelations;
@@ -51,7 +55,18 @@ interface Props {
 export const InteractionCard = ({ interaction, setInteractions }: Props) => {
   const [openDialog, setOpenDialog] = useState<boolean>(false);
   const [isPending, setIsPending] = useState<boolean>(false);
-  const [attachment, setAttachment] = useState<File | null>(null);
+  const [newContent, setNewContent] = useState<string>(interaction.content);
+  const [attachmentInfo, setAttachmentInfo] = useState<Attachment | null>(
+    interaction.attachmentUrl
+      ? {
+          attachmentName: interaction.attachmentName || "Archivo adjunto",
+          attachmentType: interaction.attachmentType || "",
+          attachmentUrl: interaction.attachmentUrl,
+        }
+      : null,
+  );
+  const [isAttachmentChanged, setIsAttachmentChanged] =
+    useState<boolean>(false);
 
   const deleteInteraction = async (interactionId: string) => {
     try {
@@ -75,32 +90,145 @@ export const InteractionCard = ({ interaction, setInteractions }: Props) => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsPending(true);
-    const formData = new FormData(e.currentTarget);
+
+    const formData = new FormData();
+    formData.append("content", newContent);
+
+    // Si hay un adjunto y ha sido cambiado, añadir la info del adjunto al formData
+    if (attachmentInfo && isAttachmentChanged) {
+      formData.append("attachment", JSON.stringify(attachmentInfo));
+    }
+
     try {
       const interactionUpdated = await editInteractionById(
         interaction.id,
         formData,
       );
+
       if (!interactionUpdated) {
         toast.error("La interaccion no se pudo completar");
         return;
       }
+
+      // Actualizar el estado local con los datos actualizados
       setInteractions((prevItems) =>
         prevItems.map((item) =>
           item.id === interaction.id
-            ? { ...item, ...interactionUpdated }
+            ? {
+                ...item,
+                content: newContent,
+                attachmentUrl: attachmentInfo?.attachmentUrl || null,
+                attachmentName: attachmentInfo?.attachmentName || null,
+                attachmentType: attachmentInfo?.attachmentType || null,
+              }
             : item,
         ),
       );
 
-      toast.success("Contacto editado con exito");
+      setIsAttachmentChanged(false);
+      toast.success("Interacción editada con éxito");
     } catch (err) {
-      toast.error("Algo salio mal..");
+      console.error(err);
+      toast.error("Algo salió mal...");
     } finally {
       setIsPending(false);
       setOpenDialog(false);
     }
   };
+
+  // Manejar selección de un nuevo archivo cuando no había uno
+  const handleNewFileChange = async (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    try {
+      setIsPending(true);
+      // Crear el FormData y agregar el archivo
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const attachment = await uploadFile(formData);
+
+      if (attachment.success) {
+        setAttachmentInfo({
+          attachmentName: attachment.fileName,
+          attachmentType: attachment.fileType,
+          attachmentUrl: attachment.url,
+        });
+        setIsAttachmentChanged(true);
+        toast.success("Archivo subido correctamente");
+      } else {
+        toast.error("Error al subir el archivo");
+      }
+    } catch (error) {
+      console.error("Error al subir el archivo:", error);
+      toast.error("Error al subir el archivo");
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  // Manejar reemplazo de un archivo existente
+  const handleReplaceFileChange = async (
+    e: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    try {
+      setIsPending(true);
+      const fileKey = interaction?.attachmentUrl?.split("/").pop();
+
+      if (!fileKey) {
+        throw new Error("No se puede obtener el fileKey");
+      }
+
+      const attachment = await replaceFile(fileKey, {
+        interactionId: interaction.id,
+        file: file,
+      });
+
+      if (attachment.success) {
+        setAttachmentInfo({
+          attachmentName: attachment.fileName,
+          attachmentType: attachment.fileType,
+          attachmentUrl: attachment.url,
+        });
+        setInteractions((prevItems) =>
+          prevItems.map((item) =>
+            item.id === interaction.id
+              ? {
+                  ...item,
+                  content: newContent,
+                  attachmentUrl: attachmentInfo?.attachmentUrl || null,
+                  attachmentName: attachmentInfo?.attachmentName || null,
+                  attachmentType: attachmentInfo?.attachmentType || null,
+                }
+              : item,
+          ),
+        );
+
+        setIsAttachmentChanged(true);
+        toast.success("Archivo reemplazado correctamente");
+      } else {
+        toast.error("Error al reemplazar el archivo");
+      }
+    } catch (error) {
+      console.error("Error al reemplazar el archivo:", error);
+      toast.error("Error al reemplazar el archivo");
+    } finally {
+      setIsPending(false);
+    }
+  };
+
+  // Eliminar adjunto actual
+  const handleRemoveAttachment = () => {
+    setAttachmentInfo(null);
+    setIsAttachmentChanged(true);
+  };
+
   return (
     <>
       <Card key={interaction.id} className="border-l-2 border-l-primary">
@@ -127,7 +255,6 @@ export const InteractionCard = ({ interaction, setInteractions }: Props) => {
                 </CardDescription>
               </div>
             </div>
-
             <div>
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
@@ -140,14 +267,14 @@ export const InteractionCard = ({ interaction, setInteractions }: Props) => {
                     onClick={() => setOpenDialog(true)}
                     className="cursor-pointer"
                   >
-                    <Edit />
+                    <Edit className="h-4 w-4 mr-2" />
                     <span>Editar</span>
                   </DropdownMenuItem>
                   <DropdownMenuItem
                     onClick={() => deleteInteraction(interaction.id)}
-                    className="text-red-500 cursor-pointer "
+                    className="text-red-500 cursor-pointer"
                   >
-                    <Trash2 />
+                    <Trash2 className="h-4 w-4 mr-2" />
                     <span>Eliminar</span>
                   </DropdownMenuItem>
                 </DropdownMenuContent>
@@ -164,6 +291,7 @@ export const InteractionCard = ({ interaction, setInteractions }: Props) => {
               href={interaction.attachmentUrl}
               download={interaction.attachmentName || true}
               className="flex items-center gap-2 text-xs text-primary hover:underline"
+              target="_blank"
             >
               <Download className="h-3 w-3" />
               {interaction.attachmentName || "Archivo adjunto"}
@@ -175,43 +303,80 @@ export const InteractionCard = ({ interaction, setInteractions }: Props) => {
       <Dialog open={openDialog} onOpenChange={setOpenDialog}>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle>Editar contacto</DialogTitle>
+            <DialogTitle>Editar Interacción</DialogTitle>
           </DialogHeader>
           <form className="space-y-4" onSubmit={handleSubmit}>
-            <div className="flex flex-col gap-4 sm:flex-row">
-              <div className="flex-1 space-y-2">
-                <Label htmlFor="nombre">Contenido</Label>
-                <Input
-                  name="content"
-                  id="content"
-                  placeholder="Contenido"
-                  type="text"
-                  required
-                  defaultValue={interaction.content}
-                />
+            <div className="space-y-2">
+              <Label htmlFor="content">Contenido</Label>
+              <Input
+                id="content"
+                value={newContent}
+                onChange={(e) => setNewContent(e.target.value)}
+                placeholder="Contenido"
+                type="text"
+                required
+              />
+            </div>
+
+            <div className="space-y-2">
+              <div className="flex items-center justify-between mb-3">
+                <Label>Archivo adjunto</Label>
               </div>
-            </div>
-            <div>
-              <Label htmlFor="attachment" className="cursor-pointer">
-                <div className="flex items-center gap-2 bg-secondary hover:bg-secondary/80 text-secondary-foreground px-3 py-2 rounded-md text-sm">
-                  <PaperclipIcon className="h-4 w-4" />
-                  {attachment ? "Cambiar archivo" : "Adjuntar archivo"}
+
+              {/* Mostrar información del archivo actual o permitir subir uno nuevo */}
+              {attachmentInfo ? (
+                <div className="space-y-2 ">
+                  <div className="flex mb-3 items-center gap-2 text-sm text-muted-foreground bg-muted/50 px-3 py-2 rounded-md">
+                    <FileText className="h-4 w-4" />
+                    <span className="truncate max-w-xs">
+                      {attachmentInfo.attachmentName}
+                    </span>
+                  </div>
+
+                  <Label htmlFor="replace-file" className="cursor-pointer">
+                    <div className="flex items-center gap-2 bg-secondary hover:bg-secondary/80 text-secondary-foreground px-3 py-2 rounded-md text-sm">
+                      <PaperclipIcon className="h-4 w-4" />
+                      Cambiar archivo
+                    </div>
+                    <input
+                      type="file"
+                      id="replace-file"
+                      className="hidden"
+                      onChange={handleReplaceFileChange}
+                      disabled={isPending}
+                    />
+                  </Label>
                 </div>
-                <input type="file" id="attachment" className="hidden" />
-              </Label>
+              ) : (
+                <Label htmlFor="new-file" className="cursor-pointer">
+                  <div className="flex items-center gap-2 bg-secondary hover:bg-secondary/80 text-secondary-foreground px-3 py-2 rounded-md text-sm">
+                    <PaperclipIcon className="h-4 w-4" />
+                    Adjuntar archivo
+                  </div>
+                  <input
+                    type="file"
+                    id="new-file"
+                    className="hidden"
+                    onChange={handleNewFileChange}
+                    disabled={isPending}
+                  />
+                </Label>
+              )}
             </div>
-            <div className="flex gap-3">
+
+            <div className="flex gap-3 pt-2">
               <Button
                 variant="outline"
                 onClick={() => setOpenDialog(false)}
                 type="button"
+                disabled={isPending}
               >
                 Cancelar
               </Button>
-              <Button type="submit">
+              <Button type="submit" disabled={isPending}>
                 {isPending ? (
                   <>
-                    <Loader2 className="size-4 mr-2 animate-spin" />
+                    <Loader2 className="h-4 w-4 mr-2 animate-spin" />
                     Cargando...
                   </>
                 ) : (
